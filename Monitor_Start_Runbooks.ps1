811feeb923697a680fc3849480b6fe620383bee2
jdk870 <jdk870@users.noreply.github.com>
2017-10-17 13:54:00 -0400

    Initial commit

---
+++ Monitor_Start_Runbooks.ps1
@@ -0,0 +1,117 @@
+﻿#Runbook names here  
+ $OpsMgrRunbooks =@('ADP Workflow', 'Exchange')  
+ #$user = 'accu\keithj'  
+ #$pass = ConvertTo-SecureString 'dog7wasa' -AsPlainText -Force  
+ #$creds = New-Object System.Management.Automation.PsCredential($user,$pass)  
+ foreach ($runbook in $OpsMgrRunbooks) {  
+   $url = "http://scorch-v01.accu.accuwx.com:81/Orchestrator2012/Orchestrator.svc/Jobs()?`$expand=Runbook&`$filter=(Runbook/Name eq '$runbook')&`$select=Runbook/Name,Status"  
+   $request = [System.Net.HttpWebRequest]::Create($url)  
+   $request.UseDefaultCredentials = $true 
+   $request.Timeout = 120000  
+   $request.ContentType = 'application/atom+xml,application/xml'  
+   $request.Headers.Add('DataServiceVersion', '2.0;NetFx')  
+   $request.Method = 'GET'  
+   $response = $request.GetResponse()  
+   $requestStream = $response.GetResponseStream()  
+   $readStream=new-object System.IO.StreamReader $requestStream  
+   $Output = $readStream.ReadToEnd()  
+   $readStream.Close()  
+   $response.Close()  
+   $Output > $env:TEMP\1.log  
+   $htmlid = Get-Content -Path $env:TEMP\1.log | Select-String -pattern '<id>.*Runbooks.*'  
+   $bookid = ($htmlid -split "'")[1]  
+   $status = $Output -match "<d:Status>Running</d:Status>"  
+   If ($Status -ne $True) {  
+#Details of the runbook we are going to run
+$rbid = $bookid  
+$rbParameters = $null
+
+#Create the request object
+$request = [System.Net.HttpWebRequest]::Create("http://scorch-v01.accu.accuwx.com:81/Orchestrator2012/Orchestrator.svc/Jobs")
+
+#Set the credentials to default or prompt for credentials
+$request.UseDefaultCredentials = $true
+#$request.Credentials = Get-Credential
+
+#Build the request header
+$request.Method = "POST"
+$request.UserAgent = "Microsoft ADO.NET Data Services"
+$request.Accept = "application/atom+xml,application/xml"
+$request.ContentType = "application/atom+xml"
+$request.KeepAlive = $true
+$request.Headers.Add("Accept-Encoding","identity")
+$request.Headers.Add("Accept-Language","en-US")
+$request.Headers.Add("DataServiceVersion","1.0;NetFx")
+$request.Headers.Add("MaxDataServiceVersion","2.0;NetFx")
+$request.Headers.Add("Pragma","no-cache")
+
+# If runbook servers are specified, format the string
+$rbServerString = ""
+if (-not [string]::IsNullOrEmpty($RunbookServers)) {
+   $rbServerString = -join ("<d:RunbookServers>",$RunbookServers,"</d:RunbookServers>")
+}
+
+# Format the Runbook parameters, if any
+$rbParamString = ""
+if ($rbParameters -ne $null) {
+   
+   # Format the param string from the Parameters hashtable
+   $rbParamString = "<d:Parameters><![CDATA[<Data>"
+   foreach ($p in $rbParameters.GetEnumerator())
+   {
+      #$rbParamString = -join ($rbParamString,"&lt;Parameter&gt;&lt;ID&gt;{",$p.key,"}&lt;/ID&gt;&lt;Value&gt;",$p.value,"&lt;/Value&gt;&lt;/Parameter&gt;")         
+      $rbParamString = -join ($rbParamString,"<Parameter><ID>{",$p.key,"}</ID><Value>",$p.value,"</Value></Parameter>")
+   }
+   $rbParamString += "</Data>]]></d:Parameters>"
+}
+
+# Build the request body
+$requestBody = @"
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<entry xmlns:d="http://schemas.microsoft.com/ado/2007/08/dataservices" xmlns:m="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata" xmlns="http://www.w3.org/2005/Atom">
+    <content type="application/xml">
+        <m:properties>
+            <d:RunbookId m:type="Edm.Guid">$rbid</d:RunbookId>
+            $rbserverstring
+            $rbparamstring
+        </m:properties>
+    </content>
+</entry>
+"@
+
+# Create a request stream from the request
+$requestStream = new-object System.IO.StreamWriter $Request.GetRequestStream()
+    
+# Sends the request to the service
+$requestStream.Write($RequestBody)
+$requestStream.Flush()
+$requestStream.Close()
+
+# Get the response from the request
+[System.Net.HttpWebResponse] $response = [System.Net.HttpWebResponse] $Request.GetResponse()
+
+# Write the HttpWebResponse to String
+$responseStream = $Response.GetResponseStream()
+$readStream = new-object System.IO.StreamReader $responseStream
+$responseString = $readStream.ReadToEnd()
+
+# Close the streams
+$readStream.Close()
+$responseStream.Close()
+
+# Get the ID of the resulting job
+if ($response.StatusCode -eq 'Created')
+{
+    $xmlDoc = [xml]$responseString
+    $jobId = $xmlDoc.entry.content.properties.Id.InnerText
+    Write-Host "Successfully started runbook. Job ID: " $jobId
+}
+else
+{
+    Write-Host "Could not start runbook. Status: " $response.StatusCode
+}
+
+}
+
+Else { "Runbook $runbook | $bookid - Already running" >> C:\Powershell\Log\RunbookManager.log } 
+}
---
+++ SKU_Automation_Script2.ps1
@@ -0,0 +1,206 @@
+﻿#-----------------------------------#
+#----Product SKU Code Generator-----#
+#-------Created By JD Keith---------#
+#-----------10/16/2017--------------#
+#-----------------------------------#
+
+
+#Orchestrator sets variables from Request offering
+$OGProductName = "Advanced Package"
+$OGPlatform = "AccuNet"
+$OGLengthofTime = "20"
+$OGUnitofTime = "Year"
+$OGDeliveryMethod = "HTTPGet"
+
+#Set Path Variables
+$PNPath = "D:\SKU Automation\ProductSKU.txt"
+$PLPath = "D:\SKU Automation\PlatformSKU.txt"
+$DMPath = "D:\SKU Automation\DeliveryMethodSKU.txt"
+
+
+#------------------------------------------#
+#-----------Start Time SKU-----------------#
+#------------------------------------------#
+
+
+#Set NA variables
+if ($OGLengthofTime -eq "NA")
+        {$LTime = "ZZ"}
+
+#If not NA, proceed with creating time length skus
+    else {
+            
+
+#Decide to insert 0 or 00
+$x = $OGLengthofTime.length
+
+if ($x -eq "1")  {$LTime = $OGLengthofTime.Insert(0, '00')}
+
+if ($x -eq "2") {$LTime = $OGLengthofTime.Insert(0,'0')}
+
+}
+
+#Same for time unit skus
+if ($OGUnitofTime -eq "NA")
+       {$UTime = "ZZ"}
+
+#If not NA, trim
+    else {
+
+#Trimming Units
+if ($OGUnitofTime -eq "Day") 
+        {$UTime = "D"}
+
+if ($OGUnitofTime -eq "Week")
+        {$UTime = "W"}
+
+if ($OGUnitofTime -eq "Month")
+        {$UTime = "M"}
+
+if ($OGUnitofTime -eq "Year")
+        {$UTime = "Y"}
+
+}
+
+#Combine Units for SKU
+$TimeValue = $LTime+$UTime
+
+#Check if Time Value is longer than 4 characters. If yes, trim to 4.
+if ($TimeValue.Length -gt "4") 
+        {$TrimValue = $TimeValue.Substring(0, [System.Math]::Min(4, $TimeValue.Length))}
+    
+    else {$TrimValue = $TimeValue
+}
+
+#Do the same if it's less than 4 characters            
+if ($TrimValue.Length -lt "4")
+        {$TrimValue = $TrimValue.Insert(0, 'Z')}
+
+$StringExists = Select-String -Path "D:\SKU Automation\TimeSKU.txt" -Pattern "$TrimValue"
+    if ($StringExists -eq $null) 
+        {Add-Content "D:\SKU Automation\TimeSKU.txt" -value $TrimValue}
+    else {$TrimValue = $StringExists.Pattern}
+
+#End Time Value SKU
+
+
+#------------------------------------------#
+#--------Start Product Name Sku------------#
+#------------------------------------------#
+
+
+#Remove spaces from product name and generate random characters (can change to alpha-numeric if needed)
+$ProductName = $OGProductName.Replace(' ','')
+$PNRAddChar = -join ((65..90) | Get-Random -Count 4 | % {[char]$_})
+
+#Find number of matching SKU objects
+$StringCount = @(Get-Content $PNPath | Where-Object { $_.Contains($ProductName) }).Count
+
+#Get matching SKU if it exists
+if 
+
+    ($StringCount -eq "1")
+        
+        {$ProductName = Get-Content $PNPath -Filter $ProductName | Where-Object {$_ -match $ProductName}
+
+        }
+
+#Generate new SKU
+else
+
+        {$ProductName = Add-Content $PNPath -value $ProductName-$PNRAddChar -PassThru
+        
+        }
+
+#Trim dash and space
+$ProductName = $ProductName.Split("-")[1].Split(" ") 
+
+#End Product Name SKU
+
+
+#------------------------------------------#
+#-----------Start Platform Sku-------------#
+#------------------------------------------#
+
+
+#Set NA variable
+if ($OGPlatform -eq "NA")
+        {$Platform = "ZZZZ"}
+
+#If not NA, proceed
+    else {
+
+$Platform = $OGPlatform.Replace(' ','')
+$PLRAddChar = -join ((65..90) | Get-Random -Count 4 | % {[char]$_})
+
+$StringCount = @(Get-Content $PLPath | Where-Object { $_.Contains($Platform) }).Count
+
+if 
+
+    ($StringCount -eq "1")
+        
+        {$Platform = Get-Content $PLPath -Filter $Platform | Where-Object {$_ -match $Platform}
+
+        }
+
+
+else
+
+        {$Platform = Add-Content $PLPath -value $Platform-$PLRAddChar -PassThru
+        
+        }
+
+$Platform = $Platform.Split("-")[1].Split(" ")
+
+}
+#End Platform SKU
+
+
+#------------------------------------------#
+#--------Start Delivery Method Sku---------#
+#------------------------------------------#
+
+
+#Set NA variable
+if ($OGDeliveryMethod -eq "NA")
+        {$DeliveryMethod = "ZZZZ"}
+
+#If not NA, proceed
+else {
+
+$DeliveryMethod = $OGDeliveryMethod.Replace(' ','')
+$DMRAddChar = -join ((65..90) | Get-Random -Count 4 | % {[char]$_})
+
+$StringCount = @(Get-Content $DMPath | Where-Object { $_.Contains($DeliveryMethod) }).Count
+
+if 
+
+    ($StringCount -eq "1")
+        
+        {$DeliveryMethod = Get-Content $DMPath -Filter $DeliveryMethod | Where-Object {$_ -match $DeliveryMethod}
+
+        }
+
+
+else
+
+        {$DeliveryMethod = Add-Content $DMPath -value $DeliveryMethod-$DMRAddChar -PassThru
+        
+        }
+
+$DeliveryMethod = $DeliveryMethod.Split("-")[1].Split(" ")
+
+}
+#End Delivery Method SKU
+
+#------------------------------------------#
+#------------Final SKU Code----------------#
+#------------------------------------------#
+
+
+#Put new SKUs together for final SKU code
+$OGSKU = "$ProductName-$Platform-$DeliveryMethod-"
+
+#Insert Dashes and Time to OG SKU
+$NEWSKU = $OGSKU.Insert(15, $TrimValue)
+Add-Content -Path "D:\SKU Automation\SKUs.csv" -Value "$OGProductName,$OGPlatform,$OGLengthOfTime,$OGUnitOfTime,$OGDeliveryMethod,$NEWSKU"
